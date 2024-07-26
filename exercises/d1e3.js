7ad135d87a549570d1b753778922ad7733848ca3
Denise Wood <deniseofarrell@gmail.com>
2024-07-26 11:46:09 +0100

    d1e3

--- exercises/d1e3.js
+++ exercises/d1e3.js
@@ -4,8 +4,9 @@ const app = express()
 app.set('views', 'exercises/views') // <-- Don't change
 
 // TODO: Configure the app to use EJS as the view engine
+app.set('view engine', 'ejs')
 
-app.get('/d1e3', (req, res) => {
+app.get('/d1e3', async (req, res) => {
   const shopping = [
     'Eggs',
     'Flour',
@@ -16,6 +17,8 @@ app.get('/d1e3', (req, res) => {
   ]
 
   // TODO: Render 'd1e3.ejs', passing the value of `shopping
+
+  res.render("d1e3", { shopping })
 })
 
 // TODO: Open d1e3.ejs and follow the instructions
